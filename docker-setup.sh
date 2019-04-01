7fb92ef91297a8e1d6e9802c77852b554573bb06
caprowsky <alessandrocapra@gmail.com>
2019-04-01 11:47:15 +0200

    Aggiunto script per impostare il proprio ambiente host con docker

---
+++ docker-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo apt install unzip
+sudo apt-get install  curl  apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+#install docker compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo usermod -a -G docker $USER
